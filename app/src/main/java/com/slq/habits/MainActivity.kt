65cf9adda45cebafb5e856fdca93b805a2d8a7f8
Slawek Plamowski <slawek.plamowski@gmail.com>
2018-04-24 23:57:44 +0200

    Basic application setup

---
+++ app/src/main/java/com/slq/habits/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.slq.habits
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
